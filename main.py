78126440c181663a212eb880a3508ab2981a0e9d
diyaammour <diya_amour@windowslive.com>
2022-11-28 23:28:35 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+import smtplib
+import datetime as dt
+import random
+import pandas
+
+MY_EMAIL = "damourburner@gmail.com"
+MY_PASSWORD = ""
+
+now = dt.datetime.now()
+today_day = now.day
+today_month = now.month
+today = (today_month, today_day)
+
+data = pandas.read_csv("birthdays.csv")
+birthdays_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in data.iterrows()}
+
+if today in birthdays_dict:
+    birthday_person = birthdays_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!!\n\n{contents}"
+        )
+
+
+
+
+
+
+
+
